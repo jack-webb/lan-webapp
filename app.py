55c8883f119a7c8c27a5e016b27267d7348a49d1
Jaffa <yo@jaffa.pw>
2019-05-11 10:24:36 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, jsonify, send_from_directory
+import subprocess
+import sys
+import json
+import asyncio
+from cachetools import cached, TTLCache
+import threading
+import time
+
+game_cache = []
+
+app = Flask(__name__)
+with open('conf.json') as json_data_file:
+    conf = json.load(json_data_file)
+
+
+@app.route('/')
+def main():
+    return send_from_directory('fe', filename='index.html')
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('fe/js', path)
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('fe/css', path)
+
+@app.route('/status', methods=['GET'])
+def get_status_messages():
+    return jsonify({'status': conf['statusMessages']})
+
+@app.route('/game', methods=['GET'])
+def get_game_status():
+    return jsonify({'games': game_cache})
+
+
+def get_server_info():
+    while (True):
+        global game_cache
+        game_cache = [build_info_object(game_server) for game_server in conf['game_servers']]
+        print('hello', file=sys.stdout)
+        time.sleep(3)
+
+def build_info_object(game_server):
+    server_info = perform_gamedig(game_server['server_instance'])
+    server_info['game'] = game_server['game']
+    # server_info['join_uri'] = game_server['server_instance']['join_uri']
+    return server_info
+
+def perform_gamedig(server):
+    gamedig_id = server['gamedig_id']
+    ip = server['ip']
+    dig = subprocess.check_output(
+        ['gamedig', '--maxAttempts', str(conf['maxAttempts']), '--type', gamedig_id, ip]).decode(sys.stdout.encoding)
+    return json.loads(dig)
+
+
+if __name__ == '__main__':
+    bg_info_thread = threading.Thread(target=get_server_info)
+    bg_info_thread.start()
+
+    app.run(debug=True)
---
+++ fe/js/main.js
@@ -0,0 +1,98 @@
+window.onload = function() {
+  var clipboard = new ClipboardJS('.copy-connection');
+  var url = new URL(window.location.href);
+  var theme = url.searchParams.get("theme");
+  if (theme == "light" || theme == "l") setLightTheme();
+  
+  var gameParent = document.getElementById("game-boxes");
+  var statusParent = document.getElementById("status-messages");
+
+  fetch('/status')
+    .then(
+      function(response) {
+        if (response.status !== 200) { console.log('Looks like there was a problem. Status Code: ' + response.status); return; }
+        response.json().then(function(data) {
+          clearStatusMessages(statusParent)
+          buildStatusMessages(statusParent, data.status)
+        });
+      }
+    );
+
+    fetch('/game')
+    .then(
+      function(response) {
+        if (response.status !== 200) { console.log('Looks like there was a problem. Status Code: ' + response.status); return; }
+        response.json().then(function(data) {
+          clearGameStatusBoxes(gameParent)
+          buildGameStatusBoxes(gameParent, data.games)
+        });
+      }
+    );
+
+    (function(){
+    fetch('/status')
+    .then(
+      function(response) {
+        if (response.status !== 200) { console.log('Looks like there was a problem. Status Code: ' + response.status); return; }
+        response.json().then(function(data) {
+          clearStatusMessages(statusParent)
+          buildStatusMessages(statusParent, data.status)
+        });
+      }
+    )
+
+    fetch('/game')
+    .then(
+      function(response) {
+        if (response.status !== 200) { console.log('Looks like there was a problem. Status Code: ' + response.status); return; }
+        response.json().then(function(data) {
+          clearGameStatusBoxes(gameParent)
+          buildGameStatusBoxes(gameParent, data.games)
+        });
+      }
+    )
+    setTimeout(arguments.callee, 2000);
+})();
+}
+
+function buildGameStatusBoxes(parentNode, data) {
+  for (var i = 0; i < data.length; i++) {
+    console.log(data[i])
+    gameInfo = document.createElement("div")
+    gameInfo.setAttribute("class", "column is-one-third-desktop")
+    gameInfo.innerHTML = tmpl("tmpl-game-box", data[i])
+    parentNode.appendChild(gameInfo);
+  }
+}
+
+function clearGameStatusBoxes(parentNode) {
+  while (parentNode.firstChild) {
+    parentNode.removeChild(parentNode.firstChild);
+  }
+}
+
+function buildStatusMessages(parentNode, data) {
+  for (var i = 0; i < data.length; i++) {
+    gameInfo = document.createElement("div")
+    gameInfo.setAttribute("class", "tile is-parent is-vertical")
+    gameInfo.innerHTML = tmpl("tmpl-status-message", data[i])
+    parentNode.appendChild(gameInfo);
+  }
+}
+
+function clearStatusMessages(parentNode) {
+  while (parentNode.firstChild) {
+    parentNode.removeChild(parentNode.firstChild);
+  }
+}
+
+function setLightTheme() {
+  // load stock (light) bulma over the dark theme
+  // lazy, but it works (because they style the same attributes & elements)
+  var newlink = document.createElement("link");
+  newlink.setAttribute("rel", "stylesheet");
+  newlink.setAttribute("type", "text/css");
+  newlink.setAttribute("href", "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css");
+  document.getElementsByTagName("head").item(0).appendChild(newlink)
+}
+// [csgo, tf2, minecraft_survival, minecraft_creative, quake_live, factorio]
